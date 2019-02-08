c26aacbc461013afc69ef684eeff40b474c0af90
David Kester <dkkester@amazon.com>
2019-02-08 15:25:41 -0800

    better JSON handling, but still sucks

--- src/FifthEditionAPI.java
+++ src/FifthEditionAPI.java
@@ -1,4 +1,6 @@
 import com.google.gson.Gson;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,7 +28,7 @@ public class FifthEditionAPI {
 
         try {
 
-            URL url = new URL("http://www.dnd5eapi.co/api/spells/1");
+            URL url = new URL("http://www.dnd5eapi.co/api/spells/1/?format=json");
             HttpURLConnection openConnection = (HttpURLConnection) url.openConnection();
             openConnection.setRequestMethod("GET");
             openConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
@@ -35,22 +37,25 @@ public class FifthEditionAPI {
                 throw new RuntimeException("HTTP : " + openConnection.getResponseCode());
             }
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
+            BufferedReader httpResponse = new BufferedReader(new InputStreamReader(
                     (openConnection.getInputStream())));
 
-            String output;
-            Gson gson = new Gson();
+            String httpResponseString;
+            StringBuffer response = new StringBuffer();
 
             System.out.println("Output from Server .... \n");
 
-            while ((output = reader.readLine()) != null) {
-                output += reader.readLine();
-                System.out.println(output);
+            while ((httpResponseString = httpResponse.readLine()) != null) {
+                response.append(httpResponseString);
             }
-            gson.toJson(output, Spell.class);
-            openConnection.disconnect();
+            httpResponse.close();
 
-            System.out.println("//============Gson: " + gson);
+            System.out.println(response.toString());
+            JSONObject myResponse = new JSONObject(response.toString());
+            System.out.println("index: " + myResponse.getString("index"));
+            System.out.println("name: " + myResponse.getString("name"));
+            System.out.println("school: " + myResponse.get("school"));
+            openConnection.disconnect();
 
         } catch (MalformedURLException e) {
 
@@ -60,6 +65,8 @@ public class FifthEditionAPI {
 
             e.printStackTrace();
 
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
     }
