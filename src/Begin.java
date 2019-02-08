05c084486836de29dfa52b2ab3c77f5a87856ae1
David Kester <dkkester@amazon.com>
2019-02-08 08:41:04 -0800

    Adding gson

--- src/Begin.java
+++ src/Begin.java
@@ -20,45 +20,45 @@ import javax.swing.*;
 public class Begin extends javax.swing.JFrame {
 
     public static void main(String[] args) throws JSONException, IOException {
-        Character Loeb = new Character(RaceChoice.HUMAN, Classes.FIGHTER, Gender.MALE);
-        Loeb.setCharacterName("Loeb");
-
-        for(int i = 0; i < 5; i++) {
-            System.out.println();
-        }
-        System.out.println("STR " + Loeb.getStrength() + " Mod " + Loeb.getStrengthModifier());
-        System.out.println("DEX " + Loeb.getDexterity() + " Mod " + Loeb.getDexterityModifier());
-        System.out.println("CON " + Loeb.getConstitution() + " Mod " + Loeb.getConstitutionModifier());
-        System.out.println("INT " + Loeb.getIntelligence() + " Mod " + Loeb.getIntelligenceModifier());
-        System.out.println("WIS " + Loeb.getWisdom() + " Mod " + Loeb.getWisdomModifier());
-        System.out.println("CHA " + Loeb.getCharisma() + " Mod " + Loeb.getCharismaModifier());
-        System.out.println();
-        System.out.println("HP " + Loeb.getMaximumHitPoints());
-        System.out.println();
-        System.out.println("Fortitude Save " + Loeb.getFortitudeSavingThrow());
-        System.out.println("Reflex Save " + Loeb.getReflexSavingThrow());
-        System.out.println("Will Save " + Loeb.getWillSavingThrow());
-        System.out.println("Base Attack Bonus " + Loeb.getBaseAttackBonus());
-        System.out.println();
-        System.out.println("Initiative Mod " + Loeb.getInitiative());
-        // Skills
-        System.out.println("Total Skill Points = " + Loeb.getSkillPoints());
-        Loeb.firstTimeRankSkill(Skills.CLIMB, 3);
-        Loeb.firstTimeRankSkill(Skills.CONCENTRATION, 4);
-        System.out.println("Remaining Skill Points " + Loeb.getSkillPoints());
-        System.out.println(Loeb.getSkillandRank(Skills.CONCENTRATION));
-        System.out.println(Loeb.getSkillandRank(Skills.CLIMB));
-        System.out.println(Loeb.getSkillandRank(Skills.INTIMIDATE));
-        // Feats
-        System.out.println("Unallocated Feats = " + Loeb.getNumberOfFeats());
-        ArrayList<Feats> feats = Loeb.getFeats();
-        for (Feats feat : feats) {
-            System.out.println("Feat: " + feat);
-        }
-        // ArmorList JobClass
-        System.out.println("Armor Class = " + Loeb.getArmorClass());
-        // Equipped Weapon
-        System.out.println("Equipped Weapon: " + Loeb.getEquippedWeapon());
+//        Character Loeb = new Character(RaceChoice.HUMAN, Classes.FIGHTER, Gender.MALE);
+//        Loeb.setCharacterName("Loeb");
+//
+//        for(int i = 0; i < 5; i++) {
+//            System.out.println();
+//        }
+//        System.out.println("STR " + Loeb.getStrength() + " Mod " + Loeb.getStrengthModifier());
+//        System.out.println("DEX " + Loeb.getDexterity() + " Mod " + Loeb.getDexterityModifier());
+//        System.out.println("CON " + Loeb.getConstitution() + " Mod " + Loeb.getConstitutionModifier());
+//        System.out.println("INT " + Loeb.getIntelligence() + " Mod " + Loeb.getIntelligenceModifier());
+//        System.out.println("WIS " + Loeb.getWisdom() + " Mod " + Loeb.getWisdomModifier());
+//        System.out.println("CHA " + Loeb.getCharisma() + " Mod " + Loeb.getCharismaModifier());
+//        System.out.println();
+//        System.out.println("HP " + Loeb.getMaximumHitPoints());
+//        System.out.println();
+//        System.out.println("Fortitude Save " + Loeb.getFortitudeSavingThrow());
+//        System.out.println("Reflex Save " + Loeb.getReflexSavingThrow());
+//        System.out.println("Will Save " + Loeb.getWillSavingThrow());
+//        System.out.println("Base Attack Bonus " + Loeb.getBaseAttackBonus());
+//        System.out.println();
+//        System.out.println("Initiative Mod " + Loeb.getInitiative());
+//        // Skills
+//        System.out.println("Total Skill Points = " + Loeb.getSkillPoints());
+//        Loeb.firstTimeRankSkill(Skills.CLIMB, 3);
+//        Loeb.firstTimeRankSkill(Skills.CONCENTRATION, 4);
+//        System.out.println("Remaining Skill Points " + Loeb.getSkillPoints());
+//        System.out.println(Loeb.getSkillandRank(Skills.CONCENTRATION));
+//        System.out.println(Loeb.getSkillandRank(Skills.CLIMB));
+//        System.out.println(Loeb.getSkillandRank(Skills.INTIMIDATE));
+//        // Feats
+//        System.out.println("Unallocated Feats = " + Loeb.getNumberOfFeats());
+//        ArrayList<Feats> feats = Loeb.getFeats();
+//        for (Feats feat : feats) {
+//            System.out.println("Feat: " + feat);
+//        }
+//        // ArmorList JobClass
+//        System.out.println("Armor Class = " + Loeb.getArmorClass());
+//        // Equipped Weapon
+//        System.out.println("Equipped Weapon: " + Loeb.getEquippedWeapon());
 
         FifthEditionAPI.callDnd5eAPI();
 //        CreationWindow window = new CreationWindow(Loeb);
--- src/FifthEditionAPI.java
+++ src/FifthEditionAPI.java
@@ -1,3 +1,5 @@
+import com.google.gson.Gson;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,12 +9,24 @@ import java.net.URL;
 
 public class FifthEditionAPI {
 
+    private class Spell {
+        private Integer index;
+        private String name;
+
+        @Override
+        public String toString() {
+            return index + " " + name;
+        }
+    }
+
+
+
     // http://localhost:8080/RESTfulExample/json/product/get
     public static void callDnd5eAPI() {
 
         try {
 
-            URL url = new URL("http://www.dnd5eapi.co/api/skills/");
+            URL url = new URL("http://www.dnd5eapi.co/api/spells/1");
             HttpURLConnection openConnection = (HttpURLConnection) url.openConnection();
             openConnection.setRequestMethod("GET");
             openConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
@@ -25,12 +39,18 @@ public class FifthEditionAPI {
                     (openConnection.getInputStream())));
 
             String output;
+            Gson gson = new Gson();
+
             System.out.println("Output from Server .... \n");
+
             while ((output = reader.readLine()) != null) {
+                output += reader.readLine();
                 System.out.println(output);
             }
-
+            gson.toJson(output, Spell.class);
             openConnection.disconnect();
+
+            System.out.println("//============Gson: " + gson);
 
         } catch (MalformedURLException e) {
 
